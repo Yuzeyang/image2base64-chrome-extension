b52d7d4b0360eb3733217e625d3c40598d995469
Zeeyang <ios.yuzeyang@gmail.com>
2020-09-24 23:53:38 +0800

    1.【add】上传组件demo 2.【add】gitignore

--- src/popup/src/App.tsx
+++ src/popup/src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Uploader from './Uploader/index';
+// const imageToBase64 = require('image-to-base64');
 
 function App() {
+  
+  const onConvertClick = (path: string) => {
+    console.log('yzy');
+    // imageToBase64(path)
+    //   .then((response: string) => {
+    //     console.log(JSON.stringify(response));
+    //   })
+    //   .catch((error: string) => {
+    //     console.error(JSON.stringify(error));
+    //   });
+  }
+
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React11
+    //     </a>
+    //     <div onClick={() => { console.log('111') }}>click11</div>
+    //   </header>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Uploader />
     </div>
   );
 }
