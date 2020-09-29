cd99966e50ea0249d0b0ffe66c1249178d33d34a
Zeeyang <ios.yuzeyang@gmail.com>
2020-09-29 11:47:58 +0800

    1.【add】功能组件及样式

--- src/popup/src/App.tsx
+++ src/popup/src/App.tsx
@@ -1,42 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Uploader from './Uploader/index';
+import { Upload, message, Button } from 'antd';
+import 'antd/dist/antd.css';
+import { InboxOutlined, SyncOutlined } from '@ant-design/icons';
 // const imageToBase64 = require('image-to-base64');
 
 function App() {
-  
-  const onConvertClick = (path: string) => {
-    console.log('yzy');
-    // imageToBase64(path)
-    //   .then((response: string) => {
-    //     console.log(JSON.stringify(response));
-    //   })
-    //   .catch((error: string) => {
-    //     console.error(JSON.stringify(error));
-    //   });
-  }
+  const { Dragger } = Upload;
+  let imageBase64Data = '';
+  const props = {
+    name: 'file',
+    multiple: true,
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React11
-    //     </a>
-    //     <div onClick={() => { console.log('111') }}>click11</div>
-    //   </header>
-    // </div>
     <div className="App">
-      <Uploader />
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+          band files
+        </p>
+      </Dragger>
+      <textarea className="text-area" placeholder={'here is image base64 data'} rows={5}>{imageBase64Data}</textarea>
+      <Button className="transform-button" type="primary" shape="round" icon={<SyncOutlined />} size={'large'}>
+        Transform
+      </Button>
     </div>
   );
 }
